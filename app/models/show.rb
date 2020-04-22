103dba40cc516cca558c2624d1312639ef88c28c
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-22 05:30:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end 
   
   def Show::most_popular_show
-    Show.where("rating = ?", Show.highest_rating)
+    Show.rating.where("rating = ?", Show.highest_rating)
     binding.pry
   end 
   
